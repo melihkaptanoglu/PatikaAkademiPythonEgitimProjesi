9d97e9e174a57ee59d61b51eb23773fe24e1004b
Melih <melihberkkaptanoglu@gmail.com>
2025-02-04 23:47:14 +0300

    reverseandflatten.py
    Patika Akademi Python Eğitimi bitirme projesi

--- reverseandflatten.py
+++ reverseandflatten.py
@@ -1,6 +1,6 @@
 #1- Bir listeyi düzleştiren (flatten) fonksiyon yazın. Elemanları birden çok katmanlı listelerden ([[3],2] gibi) oluşabileceği
-gibi,non-scalar verilerden de oluşabilir. 
-Örnek olarak:
+#gibi,non-scalar verilerden de oluşabilir. 
+#Örnek olarak:
 
 sample_list_1 = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 
